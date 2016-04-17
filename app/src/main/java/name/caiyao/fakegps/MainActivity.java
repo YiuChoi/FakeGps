3ff9c8d0474f8838189011768c0aaf7470db1747
‘caiyao’ <whucaiyao@gmail.com>
2016-04-18 00:13:01 +0800

    Initial commit

---
+++ app/src/main/java/name/caiyao/fakegps/MainActivity.java
@@ -0,0 +1,131 @@
+package name.caiyao.fakegps;
+
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.MapView;
+import com.amap.api.maps2d.model.LatLng;
+import com.amap.api.maps2d.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements AMap.OnMapClickListener, LocationListener {
+
+    private MapView mv;
+    private AMap aMap;
+    private LocationManager locationManager;
+    private String mMockProviderName = LocationManager.PASSIVE_PROVIDER;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mv = (MapView) findViewById(R.id.mv);
+        assert mv != null;
+        mv.onCreate(savedInstanceState);
+        aMap = mv.getMap();
+        aMap.setMapType(AMap.MAP_TYPE_NORMAL);
+        aMap.setOnMapClickListener(this);
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        String provider = locationManager.getBestProvider(criteria, true);
+        if (provider != null) {
+            mMockProviderName = provider;
+        }
+
+        try {
+            locationManager.addTestProvider(mMockProviderName, false, true, false, false, true, true,
+                    true, 0, 5);
+            locationManager.setTestProviderEnabled(mMockProviderName, true);
+            locationManager.requestLocationUpdates(mMockProviderName, 0, 0, this);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, "请打开模拟位置权限！", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mv.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mv.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mv.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mv.onDestroy();
+    }
+
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        aMap.clear();
+        Log.i("TAG", latLng.latitude + ":" + latLng.longitude);
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        markerOptions.draggable(true);
+        markerOptions.title("经度：" + latLng.longitude + ",纬度：" + latLng.latitude);
+        aMap.addMarker(markerOptions);
+        Location location = new Location(mMockProviderName);
+        location.setTime(System.currentTimeMillis());
+        location.setLatitude(latLng.latitude);
+        location.setLongitude(latLng.longitude);
+        location.setAltitude(2.0f);
+        location.setAccuracy(3.0f);
+        location.setElapsedRealtimeNanos(SystemClock.elapsedRealtimeNanos());
+        try {
+            locationManager.setTestProviderLocation(mMockProviderName, location);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "请打开模拟位置权限！", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        double lat = location.getLatitude();
+        double lng = location.getLongitude();
+        Log.i("gps", String.format("location: x=%s y=%s", lat, lng));
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
---
+++ app/src/main/java/name/caiyao/fakegps/MainHook.java
@@ -0,0 +1,42 @@
+package name.caiyao.fakegps;
+
+import android.content.ContentResolver;
+import android.os.Build;
+import android.provider.Settings;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+/**
+ * Created by 蔡小木 on 2016/4/17 0017.
+ */
+public class MainHook implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+        if (loadPackageParam.packageName.equals("com.tencent.mm")) {
+            XposedHelpers.findAndHookMethod("android.provider.Settings.Secure", loadPackageParam.classLoader, "getString",
+                    ContentResolver.class, String.class, new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            String requested = (String) param.args[1];
+                            if (requested.equals(Settings.Secure.ALLOW_MOCK_LOCATION)) {
+                                param.setResult("0");
+                            }
+                        }
+                    });
+
+            // at API level 18, the function Location.isFromMockProvider is added
+            if (Build.VERSION.SDK_INT >= 18) {
+                XposedHelpers.findAndHookMethod("android.location.Location", loadPackageParam.classLoader, "isFromMockProvider",
+                        new XC_MethodHook() {
+                            @Override
+                            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                                param.setResult(false);
+                            }
+                        });
+            }
+        }
+    }
+}
