2c197d0b726b9c3cd109c8e60dfe4ec6729a08a4
YiuChoi <whucaiyao@gmail.com>
2016-04-21 20:18:03 +0800

    填家选项设置，准备发布

--- app/src/main/java/name/caiyao/fakegps/MainActivity.java
+++ app/src/main/java/name/caiyao/fakegps/MainActivity.java
@@ -1,35 +1,48 @@
 package name.caiyao.fakegps;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.CameraUpdateFactory;
 import com.amap.api.maps2d.MapView;
 import com.amap.api.maps2d.model.LatLng;
 import com.amap.api.maps2d.model.MarkerOptions;
+import com.amap.api.services.core.PoiItem;
+import com.amap.api.services.poisearch.PoiResult;
+import com.amap.api.services.poisearch.PoiSearch;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements AMap.OnMapClickListener, LocationListener {
 
     private MapView mv;
     private AMap aMap;
     private LatLng latLng;
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        sharedPreferences = getSharedPreferences("locationHistory", Context.MODE_PRIVATE);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -37,6 +50,12 @@ public class MainActivity extends AppCompatActivity implements AMap.OnMapClickLi
         assert mv != null;
         mv.onCreate(savedInstanceState);
         aMap = mv.getMap();
+        double lat = Double.parseDouble(sharedPreferences.getString("lat", "0.0"));
+        double lon = Double.parseDouble(sharedPreferences.getString("lon", "0.0"));
+        if (lat != 0.0 && lon != 0.0){
+            aMap.moveCamera(CameraUpdateFactory.changeLatLng(new LatLng(lat, lon)));
+            aMap.moveCamera(CameraUpdateFactory.zoomTo(aMap.getMaxZoomLevel()));
+        }
         aMap.setMapType(AMap.MAP_TYPE_NORMAL);
         aMap.setOnMapClickListener(this);
     }
@@ -79,6 +98,7 @@ public class MainActivity extends AppCompatActivity implements AMap.OnMapClickLi
                     Toast.makeText(this, "请点击地图选择一个地点！", Toast.LENGTH_SHORT).show();
                     return true;
                 }
+                sharedPreferences.edit().putString("lat", String.valueOf(latLng.latitude)).putString("lon", String.valueOf(latLng.longitude)).apply();
                 LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
                 try {
                     String mockProviderName = LocationManager.GPS_PROVIDER;
@@ -102,6 +122,23 @@ public class MainActivity extends AppCompatActivity implements AMap.OnMapClickLi
             case R.id.stop:
                 startService(new Intent(MainActivity.this, MockGpsService.class).putExtra("action", MockGpsService.ACTION_STOP));
                 break;
+            case R.id.search:
+                View view = LayoutInflater.from(this).inflate(R.layout.dialog_search, null, false);
+                final EditText et_key = (EditText) view.findViewById(R.id.key);
+                new AlertDialog.Builder(this).setView(view)
+                        .setTitle("搜索位置")
+                        .setPositiveButton("搜索", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                search(et_key.getText().toString());
+                            }
+                        }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).show();
+                break;
             case R.id.donate:
                 startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://qr.alipay.com/apoy1zw1o2xpc7915d")));
                 break;
@@ -117,6 +154,46 @@ public class MainActivity extends AppCompatActivity implements AMap.OnMapClickLi
     protected void onPause() {
         super.onPause();
         mv.onPause();
+    }
+
+    private void search(final String key) {
+        PoiSearch.Query query = new PoiSearch.Query(key, null, null);
+        query.setPageSize(10);
+        query.setPageNum(0);
+        PoiSearch poiSearch = new PoiSearch(this, query);
+        poiSearch.setOnPoiSearchListener(new PoiSearch.OnPoiSearchListener() {
+            @Override
+            public void onPoiSearched(PoiResult poiResult, int i) {
+                if (i == 1000) {
+                    final ArrayList<PoiItem> poiItems = poiResult.getPois();
+                    if (poiItems.size() != 0) {
+                        String[] keyList = new String[poiItems.size()];
+                        for (int j = 0; j < poiItems.size(); j++) {
+                            keyList[j] = poiItems.get(j).getTitle();
+                        }
+                        new AlertDialog.Builder(MainActivity.this)
+                                .setTitle("选择位置")
+                                .setSingleChoiceItems(keyList, 0, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        aMap.moveCamera(CameraUpdateFactory.changeLatLng(new LatLng(poiItems.get(which).getLatLonPoint().getLatitude(), poiItems.get(which).getLatLonPoint().getLongitude())));
+                                        aMap.moveCamera(CameraUpdateFactory.zoomTo(aMap.getMaxZoomLevel()));
+                                        dialog.dismiss();
+                                    }
+                                }).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "没有搜索结果", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }
+
+            @Override
+            public void onPoiItemSearched(PoiItem poiItem, int i) {
+
+            }
+        });
+        poiSearch.searchPOIAsyn();
     }
 
     @Override
