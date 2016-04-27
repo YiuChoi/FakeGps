e98c781c0f8a6fc660a413e37e8154d14ed2d4d3
YiuChoi <whucaiyao@gmail.com>
2016-04-27 13:36:25 +0800

    添加基站信息

--- app/src/main/java/name/caiyao/fakegps/MainHook.java
+++ app/src/main/java/name/caiyao/fakegps/MainHook.java
@@ -3,6 +3,18 @@ package name.caiyao.fakegps;
 import android.content.ContentResolver;
 import android.os.Build;
 import android.provider.Settings;
+import android.telephony.CellIdentityCdma;
+import android.telephony.CellIdentityGsm;
+import android.telephony.CellIdentityLte;
+import android.telephony.CellIdentityWcdma;
+import android.telephony.CellInfoCdma;
+import android.telephony.CellInfoGsm;
+import android.telephony.CellInfoLte;
+import android.telephony.CellInfoWcdma;
+import android.telephony.CellLocation;
+import android.telephony.gsm.GsmCellLocation;
+
+import java.util.ArrayList;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -36,5 +48,77 @@ public class MainHook implements IXposedHookLoadPackage {
                         }
                     });
         }
+
+        XposedHelpers.findAndHookMethod("android.telephony.TelephonyManager", loadPackageParam.classLoader,
+                "getNetworkType", new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        param.setResult(0);
+                    }
+                });
+
+        XposedHelpers.findAndHookMethod("android.net.wifi.WifiManager", loadPackageParam.classLoader, "getScanResults", new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                param.setResult(new ArrayList<>());
+            }
+        });
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN) {
+            XposedHelpers.findAndHookMethod("android.telephony.TelephonyManager", loadPackageParam.classLoader,
+                    "getAllCellInfo", new XC_MethodHook() {
+                        @Override
+                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                            param.setResult(getCell(0, 0, 0, 0, 0, 0));
+                        }
+                    });
+            XposedHelpers.findAndHookMethod("android.telephony.PhoneStateListener", loadPackageParam.classLoader,
+                    "onCellLocationChanged", CellLocation.class, new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            GsmCellLocation gsmCellLocation = new GsmCellLocation();
+                            gsmCellLocation.setLacAndCid(0, 0);
+                            param.args[0] = gsmCellLocation;
+                        }
+                    });
+        }
+    }
+
+    private static ArrayList getCell(int mcc, int mnc, int lac, int cid, int sid, int networkType) {
+        ArrayList arrayList = new ArrayList();
+        CellInfoGsm cellInfoGsm = (CellInfoGsm) XposedHelpers.newInstance(CellInfoGsm.class);
+        XposedHelpers.callMethod(cellInfoGsm, "setCellIdentity", XposedHelpers.newInstance(CellIdentityGsm.class, new Object[]{Integer.valueOf(mcc), Integer.valueOf(mnc), Integer.valueOf(
+                lac), Integer.valueOf(cid)}));
+        CellInfoCdma cellInfoCdma = (CellInfoCdma) XposedHelpers.newInstance(CellInfoCdma.class);
+        XposedHelpers.callMethod(cellInfoCdma, "setCellIdentity", XposedHelpers.newInstance(CellIdentityCdma.class, new Object[]{Integer.valueOf(lac), Integer.valueOf(sid), Integer.valueOf(cid), Integer.valueOf(0), Integer.valueOf(0)}));
+        CellInfoWcdma cellInfoWcdma = (CellInfoWcdma) XposedHelpers.newInstance(CellInfoWcdma.class);
+        XposedHelpers.callMethod(cellInfoWcdma, "setCellIdentity", XposedHelpers.newInstance(CellIdentityWcdma.class, new Object[]{Integer.valueOf(mcc), Integer.valueOf(mnc), Integer.valueOf(lac), Integer.valueOf(cid), Integer.valueOf(300)}));
+        CellInfoLte cellInfoLte = (CellInfoLte) XposedHelpers.newInstance(CellInfoLte.class);
+        XposedHelpers.callMethod(cellInfoLte, "setCellIdentity", XposedHelpers.newInstance(CellIdentityLte.class, new Object[]{Integer.valueOf(mcc), Integer.valueOf(mnc), Integer.valueOf(cid), Integer.valueOf(300), Integer.valueOf(lac)}));
+        switch (networkType) {
+            case 1:
+            case 2:
+                arrayList.add(cellInfoGsm);
+                break;
+            case 13:
+                arrayList.add(cellInfoLte);
+                break;
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 12:
+            case 14:
+                arrayList.add(cellInfoCdma);
+                break;
+            case 3:
+            case 8:
+            case 9:
+            case 10:
+            case 15:
+                arrayList.add(cellInfoWcdma);
+                break;
+        }
+        return arrayList;
     }
 }
